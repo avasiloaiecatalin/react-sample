429fe2171be560ff589f4252c1faaa52fdcfb0c8
avasiloaiecatalin <48545737+avasiloaiecatalin@users.noreply.github.com>
2025-01-19 20:19:22 +0200

    Update ReactQueryCreate.jsx

--- src/components/fragments/ReactQueryCreate.jsx
+++ src/components/fragments/ReactQueryCreate.jsx
@@ -40,25 +40,28 @@ export const ReactQueryCreate = () => {
               value={values.name}
               placeholder='Name'
             />
+            <br />
             <Input
               name='job'
               onChange={handleChange}
               value={values.job}
               placeholder='Job'
             />
-            <Button
-              type='submit'
-              disabled={isSubmitting}
-              className='mt-4'
-            >
-              Submit
-            </Button>
-            <Button
-              type='reset'
-              className='mt-4'
-            >
-              Reset
-            </Button>
+            <div className='flex gap-2'>
+              <Button
+                type='submit'
+                disabled={isSubmitting}
+                className='mt-4'
+              >
+                Submit
+              </Button>
+              <Button
+                type='reset'
+                className='mt-4'
+              >
+                Reset
+              </Button>
+            </div>
           </Form>
         )}
       </Formik>
@@ -66,6 +69,86 @@ export const ReactQueryCreate = () => {
       <CodeBlock
         text={JSON.stringify(apiResponse, null, 2)}
         language='json'
+      />
+      <h4>React Mutation call inside a component</h4>
+      <CodeBlock
+        text={`import { postData } from '@/lib/axios';
+import { queryClient } from '@/main';
+import { Form, Formik } from 'formik';
+import { useMutation } from 'react-query';
+import { Input } from '../ui/input';
+import { Button } from '../ui/button';
+import { useState } from 'react';
+import { CodeBlock } from 'react-code-blocks';
+
+export const ReactQueryCreate = () => {
+  const [apiResponse, setApiResponse] = useState();
+  const { mutateAsync } = useMutation(
+    async (data) => postData('/users', data),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries('users');
+      }
+    }
+  );
+
+  return (
+    <>
+      <Formik
+        initialValues={{
+          name: '',
+          job: ''
+        }}
+        onSubmit={async (values, { resetForm }) => {
+          const response = await mutateAsync(values);
+          console.log('res: ', response);
+          setApiResponse(response?.data);
+          resetForm();
+        }}
+      >
+        {({ handleSubmit, handleChange, values, isSubmitting }) => (
+          <Form onSubmit={handleSubmit}>
+            <Input
+              name='name'
+              onChange={handleChange}
+              value={values.name}
+              placeholder='Name'
+            />
+            <br />
+            <Input
+              name='job'
+              onChange={handleChange}
+              value={values.job}
+              placeholder='Job'
+            />
+            <div className='flex gap-2'>
+              <Button
+                type='submit'
+                disabled={isSubmitting}
+                className='mt-4'
+              >
+                Submit
+              </Button>
+              <Button
+                type='reset'
+                className='mt-4'
+              >
+                Reset
+              </Button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+
+      <CodeBlock
+        text={JSON.stringify(apiResponse, null, 2)}
+        language='json'
+      />
+    </>
+  );
+};
+`}
+        language='jsx'
       />
     </>
   );
